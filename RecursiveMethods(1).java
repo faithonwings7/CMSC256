3dc3d770183a5d4fff7141efe45f9161b32a15a7
faithonwings7 <faithonwings7@gmail.com>
2020-11-24 14:15:48 -0500

    Add files via upload

---
+++ RecursiveMethods(1).java
@@ -0,0 +1,42 @@
+package cmsc256;
+
+/*
+*	Programmer #1:
+*	Programmer #2:
+*/
+
+public class RecursiveMethods {
+	/* 
+	 * @returns a String of character, ch. The length is determined
+	 * 			by the second parameter, length.
+	 */
+	public static String buildStringOfCharacters(char ch, int length) {
+		return "";
+	}
+	
+	/*
+	 * returns an int array that has the elements in reverse order
+	 * 			of the parameter array, nums. 
+	 * Process this recursively beginning with the last element.
+	 */
+	public static int[] reverseNumArray(int[] nums, int backIndex) {
+		return nums;
+	}
+	
+	/*
+	 * returns true if the int array parameter is sorted from smallest
+	 * 			to largest, false otherwise.
+	 * Process this recursively beginning with the first element.
+	 */
+	public static boolean isSmallestToLargest(int[] values, int firstIndex) {
+		return false;
+	}
+	
+	/* 
+	 * @returns true if the parameter String, str is a palindrome
+	 * 			false otherwise
+	 */
+	public static boolean isPalindrome(String str, int begin, int end) {
+		return false;
+	}
+}
